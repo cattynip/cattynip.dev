a4a177acce581759751da9f1074de8c74dbb3d75
cattynip <hi@cattynip.dev>
2023-11-27 00:06:17 -0500

    feat(content): add content

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,59 @@ export default function Home() {
   return (
     <main>
       <h1>Cattynip&apos;s Official Website</h1>
+
+      <div>
+        <br />
+
+        <p>
+          Hello, I am Seol SO, known as Cattynip from South Korea, and a
+          ninth-grade student at Albert College. I have been dreaming of being a
+          computer programmer, creating value for society by developing things
+          for customers. Why? Because I love Writing Codes and Building
+          Services. I always keep the phrase `Learn Anytime, Write Anywhere`. in
+          my mind to embark on the right path. To follow the phrase, I always
+          make a commit every single day.
+        </p>
+
+        <br />
+
+        <p>
+          And, as you might know, this website is not fully built. I plan to
+          build this website in 2027, when I graduate from highschool, Albert
+          College. This website will be a website that introduces about ME, my
+          vision and dream, and projects that I have launched so far, but with
+          fancy animations and interactions. You can simply click the links
+          below to know more about me.
+        </p>
+
+        <br />
+
+        <a href="https://github.com/cattynip" target="_blank">
+          GitHub : @cattynip
+        </a>
+
+        <br />
+
+        <a href="https://instagram.com/cattynip.dev">
+          Instagram : @cattynip.dev
+        </a>
+
+        <br />
+
+        <a href="https://twitter.com/cattynip_">
+          Twitter : @cattynip_ (No tweets)
+        </a>
+
+        <br />
+
+        <a href="https://www.youtube.com/@_cattynip_">
+          YouTube : @_cattynip_ (No videos)
+        </a>
+
+        <br />
+
+        <p>Thank you so much for visiting my website.</p>
+      </div>
     </main>
   );
 }
